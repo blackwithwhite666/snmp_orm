79c16fc0f27d7158b994fee4859bca24d5a8fdad
Lipin Dmitry <blackwithwhite666@gmail.com>
2011-07-22 17:49:12 +0600

    Add some readme

--- playground.py
+++ playground.py
@@ -6,7 +6,6 @@ logging.basicConfig(level=logging.DEBUG)
 
 def main():
     d = get_device("192.168.0.225")
-    result = d.ip.ipRoutingDiscards
-    print repr(result)
+    print dict(d.system)
 
 main()
