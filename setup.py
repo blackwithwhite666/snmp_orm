4f1dc21a22a136084ece907b8ac067f887660538
Xiongfei(Alex) GUO <xfguo@credosemi.com>
2012-12-10 16:52:15 +0800

    Update setup.py info.

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from distutils.core import setup
 
-setup(name="snmp_orm",
-      version="0.1",
-      description="Python-based tool providing a simple interface to work SNMP agents",
-      author="alex",
-      author_email="supereic@gmail.com",
-      url="http://www.credosemi.com",
-      packages=['snmp_orm', 'snmp_orm.adapters', 'snmp_orm.devices'])
+setup(name = "snmp_orm",
+      version = "0.1",
+      description = "Python-based tool providing a simple interface to work SNMP agents",
+      author = "Lipin Dmitriy, Xiongfei GUO",
+      author_email = "xfguo@credosemi.com",
+      url = "https://github.com/xfguo/snmp_orm",
+      packages = ['snmp_orm', 'snmp_orm.adapters', 'snmp_orm.devices'])
